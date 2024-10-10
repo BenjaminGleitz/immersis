c6f7f1be1e3846c64d29c5e1fad6f70f7f991741
benjamingleitz <gleitz.benjamin@gmail.com>
2024-10-10 11:21:07 +0200

    immersis

---
+++ immersis/src/components/Camera.tsx
@@ -0,0 +1,77 @@
+import React, { useState } from 'react';
+import { CameraType, CameraView, useCameraPermissions } from 'expo-camera';
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+const Camera = () => {
+    const [facing, setFacing] = useState<CameraType>('back');
+    const [permission, requestPermission] = useCameraPermissions();
+
+    if (!permission) {
+        // Camera permissions are still loading.
+        return <View />;
+    }
+
+    if (!permission.granted) {
+        // Camera permissions are not granted yet.
+        return (
+            <View style={styles.permissionContainer}>
+                <Text style={styles.message}>We need your permission to show the camera</Text>
+                <Button onPress={requestPermission} title="Grant Permission" />
+            </View>
+        );
+    }
+
+    const toggleCameraFacing = () => {
+        setFacing((current) => (current === 'back' ? 'front' : 'back'));
+    };
+
+    return (
+        <View style={styles.cameraContainer}>
+            <CameraView style={styles.camera} facing={facing}>
+                <View style={styles.buttonContainer}>
+                    <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
+                        <Text style={styles.text}>Flip Camera</Text>
+                    </TouchableOpacity>
+                </View>
+            </CameraView>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    permissionContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    message: {
+        textAlign: 'center',
+        paddingBottom: 10,
+        fontSize: 18,
+    },
+    cameraContainer: {
+        flex: 1,
+        marginTop: 130, // L'espace pour la navbar
+    },
+    camera: {
+        flex: 1,
+    },
+    buttonContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        backgroundColor: 'transparent',
+        margin: 64,
+    },
+    button: {
+        flex: 1,
+        alignSelf: 'flex-end',
+        alignItems: 'center',
+    },
+    text: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: 'white',
+    },
+});
+
+export default Camera;
